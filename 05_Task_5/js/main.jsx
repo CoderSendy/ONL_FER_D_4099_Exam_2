904da7e78717bd11024a7bf6f3a5c47f2c8a9813
CoderSendy <Coderslab50@gmail.com>
2026-09-12 09:37:05 +0200

    ukol 5 final

--- 05_Task_5/js/main.jsx
+++ 05_Task_5/js/main.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+function UserDetails({ name, surname, address, postcode, city }) {
+  return (
+    <div className="card">
+      <div>
+        <h5>{name} {surname}</h5>
+        <div>{address}</div>
+        <div>{postcode} {city}</div>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <UserDetails
+      name="Sandra"
+      surname="Bartelová"
+      address="Hlavní 123"
+      postcode="500 03"
+      city="Hradec Králové"
+    />
+  );
+}
+
 /**
  * Do not modify the code below!
  */
 const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(<App />);
+
