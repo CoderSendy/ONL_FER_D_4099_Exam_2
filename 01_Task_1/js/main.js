da51caf58edcee37ec359ca36dae87f2483ac403
CoderSendy <Coderslab50@gmail.com>
2026-09-12 09:04:05 +0200

    ukol 1

--- 01_Task_1/js/main.js
+++ 01_Task_1/js/main.js
@@ -3,3 +3,12 @@ const data = [1, 2, 3, 4, 5];
 /**
  * Write your code below!
  */
+const without = (data, ...numbersToRemove) => {
+  return data.filter(number => !numbersToRemove.includes(number));
+};
+
+console.log(without(data, 1, 2));
+console.log(without(data, 5, 3));
+console.log(without(data, 1, 2, 3, 4));
+console.log(without(data));
+console.log(without(data, 1, 2, 3, 4, 5));
