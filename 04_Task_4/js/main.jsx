ff57f38531b5ca214f4f14a30fe34b3dec0abb56
CoderSendy <Coderslab50@gmail.com>
2026-09-12 09:32:35 +0200

    ukol 4 final

--- 04_Task_4/js/main.jsx
+++ 04_Task_4/js/main.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 
+function App() {
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <span>React Rules!</span>
+        </>
+    );
+}
 
 
 
