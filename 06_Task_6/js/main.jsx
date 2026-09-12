528f779f3775d27c99ef57e9eb70114af14231d9
CoderSendy <Coderslab50@gmail.com>
2026-09-12 09:40:47 +0200

    ukol 6 final

--- 06_Task_6/js/main.jsx
+++ 06_Task_6/js/main.jsx
@@ -1,6 +1,35 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import products from "./data/products.js";
 
+function Item({ name, price }) {
+  return (
+    <li>
+      {name}, Price: {price}
+    </li>
+  );
+}
+
+function Products({ products }) {
+  return (
+    <>
+      <h2>Products</h2>
+      <ul>
+        {products.map(product => (
+          <Item
+            key={product.id}
+            name={product.name}
+            price={product.price}
+          />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function App() {
+  return <Products products={products} />;
+}
 /**
  * Do not modify the code below!
  */
--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
    // root: './02_Task_2',
    //root: './03_Task_3',
    // root: './04_Task_4',
-   root: './05_Task_5',
-  // root: './06_Task_6',
+   // root: './05_Task_5',
+   root: './06_Task_6',
 
 })
