9d9375aea59108f06cde6a67f5ba38571596c342
CoderSendy <Coderslab50@gmail.com>
2026-09-12 09:15:16 +0200

    ukol 2 Final

--- 02_Task_2/js/main.js
+++ 02_Task_2/js/main.js
@@ -49,3 +49,17 @@ const users = [
 /**
  * Write your code below!
  */
+
+const parseUserData = (data) => {
+  return data
+    .filter(user => user.language === "French")
+    .map(({ id, firstName, lastName, email, nin }) => ({
+      id,
+      fullName: `${firstName} ${lastName}`,
+      email,
+      nin,
+    }))
+    .toSorted((a, b) => a.nin - b.nin);
+};
+
+console.log(parseUserData(users));
