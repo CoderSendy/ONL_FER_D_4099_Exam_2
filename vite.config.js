5a8dee4c2cc1b051788ca1d4e7c2a8ebcb640dfe
Luděk Roleček <ludek@rolecek.cz>
2026-09-12 07:29:36 +0200

    Add content

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Odkomentuj řádek s cestou k požadovanému cvičení.
+// Aktivní (nezakomentovaný) smí být vždy jen jeden řádek "root:".
+
+export default defineConfig({
+
+  plugins: [react()],
+
+  // === Exam Tasks ===
+
+  root: './01_Task_1',
+  // root: './02_Task_2',
+  // root: './03_Task_3',
+  // root: './04_Task_4',
+  // root: './05_Task_5',
+  // root: './06_Task_6',
+
+})
